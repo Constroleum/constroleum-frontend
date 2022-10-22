b04540098575fd4e1c9c2009015f0414eb0749fb
human-reborn <humanreborninfo@gmail.com>
2022-10-22 17:04:19 +0200

    mobile styles and animations for cover and about are done

--- src/components/layout/header/burger-menu/burger-menu.tsx
+++ src/components/layout/header/burger-menu/burger-menu.tsx
@@ -1,4 +1,4 @@
-import React  from "react";
+import React, {Fragment, useState} from "react";
 import * as styles from "./burger-menu.module.scss";
 
 type RenderProps = {
@@ -7,11 +7,25 @@ type RenderProps = {
 
 const BurgerMenu: React.FC<RenderProps> = ({ data }) => {
 
+    const [open, setOpen] = useState(false);
+
     return (
         <div className={styles.container}>
-            Burger Menu
+            {displayMenuButton()}
         </div>
     )
+
+    function displayMenu():JSX.Element {
+        return <Fragment />
+    }
+
+    function displayMenuButton():JSX.Element {
+        return (
+            <button className={styles.button} onClick={() => setOpen(prevState => !prevState)}>
+                <span className={styles.span1}>{open ? 'X' : 'O'}</span>
+            </button>
+        )
+    }
 
 }
 
