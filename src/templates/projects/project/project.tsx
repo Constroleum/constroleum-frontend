19fc68acd6f95dc60a58258c4a601c264f467561
human-reborn <humanreborninfo@gmail.com>
2022-10-26 19:14:00 +0200

    animations done in projects and project page

--- src/templates/projects/project/project.tsx
+++ src/templates/projects/project/project.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import {graphql} from "gatsby";
 import * as styles from "./project.module.scss";
 import {ProjectQuery} from "../../../../graphql-types";
 import Layout from "../../../components/layout/layout";
 import {displayImage} from "../../../global/functions/functions";
 import Slider from "./Slider/slider";
+import {gsap} from "gsap";
 
 type RenderProps = {
     data: ProjectQuery,
@@ -14,25 +15,56 @@ type RenderProps = {
 const ProjectPage:React.FC<RenderProps> = ({ data, pageContext }) => {
 
     const project = data.allDatoCmsProjectPage.nodes.find(proj => proj.id === pageContext.projectId);
+    const container = useRef();
+    const pageElements = useRef([])
+
+    const createElementsRefs = (section, index) => {
+        pageElements.current[index] = section;
+    }
+
+    useEffect(() => {
+        loadAnimation()
+    }, [])
 
     return (
         <Layout header={data.datoCmsHeader}>
-            <div className={styles.container}>
+            <div ref={container} className={styles.container}>
                 <div className={styles.sideImageContainer}>
                     {displayImage(project.mainImage, styles.sideImage, "cover")}
                 </div>
                 <div className={styles.content}>
                     <div className={styles.textContentContainer}>
-                        <h1 className={styles.title}>{project.title}</h1>
-                        <p className={styles.description}>{project.description}</p>
+                        <h1 className={styles.title} ref={(e) => createElementsRefs(e, 0)}>{project.title}</h1>
+                        <p className={styles.description} ref={(e) => createElementsRefs(e, 1)}>{project.description}</p>
                     </div>
-                    <div className={styles.sliderContainer}>
+                    <div className={styles.sliderContainer} ref={(e) => createElementsRefs(e, 2)}>
                         <Slider imageCollection={project.imagesCollection} />
                     </div>
                 </div>
             </div>
         </Layout>
     )
+
+    function loadAnimation() {
+        let animationDelay = 0;
+        let checkIfRefsAreLoaded = setInterval(() => {
+            if(typeof pageElements.current !== undefined && typeof container.current !== undefined) {
+                const tl = gsap.timeline({
+                    scrollTrigger: {
+                        trigger: container.current,
+                        pin: true,
+                        scrub: 1,
+                        invalidateOnRefresh: true
+                    }
+                })
+                pageElements.current.forEach(element => {
+                    tl.from(element, { y: container.current.offsetHeight, ease: 'Power1.easeOut', duration: 0.4 }, `-=${animationDelay}`)
+                    animationDelay += 0.1;
+                })
+                clearInterval(checkIfRefsAreLoaded)
+            }
+        }, 100);
+    }
 }
 
 
--- src/templates/projects/projects.tsx
+++ src/templates/projects/projects.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
 import {graphql} from "gatsby";
 import {ProjectsQuery} from "../../../graphql-types";
 import Layout from "../../components/layout/layout";
 import * as styles from "./projects.module.scss";
 import {displayImage} from "../../global/functions/functions";
+import {gsap} from "gsap";
 
 type RenderProps = {
     data: ProjectsQuery
@@ -11,16 +12,27 @@ type RenderProps = {
 
 const ProjectsPage:React.FC<RenderProps> = ({ data }) => {
 
+    const container = useRef();
+    const projects = useRef([])
+
+    const createProjectsRefs = (section, index) => {
+        projects.current[index] = section;
+    }
+
+    useEffect(() => {
+        loadAnimation()
+    }, [])
+
     return (
         <Layout header={data.datoCmsHeader}>
-            <div className={styles.container}>
+            <div ref={container} className={styles.container}>
                 <div className={styles.pageTitleContainer}>
                     <h1 className={styles.pageTitle}>{data.datoCmsAllProjectsPage.pageTitle}</h1>
                 </div>
                 <div className={styles.projectsListContainer}>
                     <ul className={styles.projectsList}>
-                        {data.datoCmsAllProjectsPage.projectsList.map(project => (
-                            <li className={styles.project}>
+                        {data.datoCmsAllProjectsPage.projectsList.map((project, index) => (
+                            <li ref={(e) => createProjectsRefs(e, index)} className={styles.project}>
                                 <a className={styles.projectLink} href={`/projects/${project.slug}`}>
                                     {project.title}
                                 </a>
@@ -33,6 +45,27 @@ const ProjectsPage:React.FC<RenderProps> = ({ data }) => {
             </div>
         </Layout>
     )
+
+    function loadAnimation() {
+        let animationDelay = 0;
+        let checkIfRefsAreLoaded = setInterval(() => {
+            if(typeof projects.current !== undefined && typeof container.current !== undefined) {
+                const tl = gsap.timeline({
+                    scrollTrigger: {
+                        trigger: container.current,
+                        pin: true,
+                        scrub: 1,
+                        invalidateOnRefresh: true
+                    }
+                })
+                projects.current.forEach((project, index) => {
+                    tl.from(project, { y: container.current.offsetHeight, ease: 'Power1.easeOut', duration: 0.6 }, `-=0.3`)
+                    animationDelay += 0.1;
+                })
+                clearInterval(checkIfRefsAreLoaded)
+            }
+        }, 100);
+    }
 }
 
 export const pageQuery = graphql`
